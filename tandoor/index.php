6fb65f8e26d87fedbef37044dbc1b1c8f0b33f90
dennisstinauer <dennisstinauer@gmail.com>
2023-07-20 19:36:50 +0200

    feat(tandoor): bugfix

--- tandoor/index.php
+++ tandoor/index.php
@@ -1,9 +1,8 @@
 <?php
 require_once '../.config.php';
 
-if ( file_exists( $dyn_dns_txt ) ) {
-
-		$dynip = file_get_contents( $dyn_dns_txt );
+if ( file_exists( '../' . $dyn_dns_txt ) ) {
+		$dynip = file_get_contents( '../' . $dyn_dns_txt );
 		$url   = 'http://' . $dynip . ':666';
 		header( "Location: $url" );
 }
