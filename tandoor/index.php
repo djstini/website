78cd93a8d2b49752ae90c9b9ac638c2d2b6f350d
dennisstinauer <dennisstinauer@gmail.com>
2023-07-20 19:34:16 +0200

    feat(tandoor): added tandoor linkOC

---
+++ tandoor/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once '../.config.php';
+
+if ( file_exists( $dyn_dns_txt ) ) {
+
+		$dynip = file_get_contents( $dyn_dns_txt );
+		$url   = 'http://' . $dynip . ':666';
+		header( "Location: $url" );
+}
