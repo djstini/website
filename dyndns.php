3b2b3f0b821b25bc923cc7b3445675f9bdb3e11a
dennisstinauer <dennisstinauer@gmail.com>
2023-07-20 18:06:49 +0200

    feat(dyndns): added dyndns service

---
+++ dyndns.php
@@ -0,0 +1,22 @@
+<?php
+require_once '../.config.php';
+$pwortcontrol = $_GET['pass'];
+$ip           = $_GET['meineip'];
+$fw           = $_GET['FW'];
+if ( file_exists( $dyn_dns_txt ) ) {
+	if ( $pwortcontrol == $dyn_dns_password ) {
+		$a = fopen( "$dyn_dns_txt", 'w' );
+		fwrite( $a, $ip );
+		fclose( $a );
+	} else {
+		$a     = fopen( "$dyn_dns_txt", 'r+' );
+		$dynip = fread( $a, filesize( $dyn_dns_txt ) );
+		fclose( $a );
+		if ( $fw == 1 ) {
+			$url = 'https://' . $dynip;
+		} else {
+			$url = 'http://' . $dynip . '' . $dyn_dns_port;
+		}
+		header( "Location: $url" );
+	}
+}
