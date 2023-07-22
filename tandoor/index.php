e198aaeb88d858a93841adc55b776192fdc53949
dennisstinauer <dennisstinauer@gmail.com>
2023-07-22 09:37:07 +0200

    feat(dyndns): add uri to redirect.

--- tandoor/index.php
+++ tandoor/index.php
@@ -1,8 +1,9 @@
 <?php
 require_once '../.config.php';
+$uri = str_replace( '/tandoor', '', $_SERVER[ REQUEST_URI ] );
 
 if ( file_exists( '../' . $dyn_dns_txt ) ) {
 		$dynip = file_get_contents( '../' . $dyn_dns_txt );
-		$url   = 'http://' . $dynip . ':666';
+		$url   = 'http://' . $dynip . ':666' . $uri;
 		header( "Location: $url" );
 }
