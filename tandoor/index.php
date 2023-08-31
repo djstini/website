d9ffd15f4d1bf0c2783f3651b8e5e914b20a7404
dennisstinauer <dennisstinauer@gmail.com>
2023-08-31 19:46:18 +0200

    feat(dyndns): removed dyndns

--- tandoor/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../.config.php';
-$uri = str_replace( '/tandoor', '', $_SERVER[ REQUEST_URI ] );
-
-if ( file_exists( '../' . $dyn_dns_txt ) ) {
-		$dynip = file_get_contents( '../' . $dyn_dns_txt );
-		$url   = 'http://' . $dynip . ':666' . $uri;
-		header( "Location: $url" );
-}
